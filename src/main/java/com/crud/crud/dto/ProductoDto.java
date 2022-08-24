8532b3d7f7fba9c656143817b025e1d45ebe47b8
ander-gil <anderson.gil@correounivalle.edu.co>
2022-08-23 23:12:09 -0500

    parte final de backend

--- src/main/java/com/crud/crud/dto/ProductoDto.java
+++ src/main/java/com/crud/crud/dto/ProductoDto.java
@@ -1,16 +1,19 @@
 package com.crud.crud.dto;
 
+import lombok.Data;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
+@Data
 public class ProductoDto {
 
     @NotBlank
     private  String nombre;
     @Min(0)
-    private float precio;
+    private Float precio;
 
-    public ProductoDto(String nombre, float precio) {
+    public ProductoDto(String nombre, Float precio) {
         this.nombre = nombre;
         this.precio = precio;
     }
